753ee4b6415c26e4d64a5887289c2fdfa418c478
TheCrusty <zezoku@yahoo.com>
2023-09-29 21:41:18 -0400

    added boot push clown minigame

---
+++ MiniGame.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var mouseIsOver = false
+var dragging = false
+
+func _physics_process(delta):
+	if(Input.is_action_pressed("LeftClick")):
+		if(mouseIsOver):
+			dragging = true
+	else:
+		dragging = false
+	
+	if(dragging):
+		$Boot.apply_central_force(get_global_mouse_position() - $Boot.global_position)
+
+func _on_boot_mouse_entered():
+	mouseIsOver = true
+
+func _on_boot_mouse_exited():
+	mouseIsOver = false
