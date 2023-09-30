e2cdba039bbba41a0b4001a375f63d767dc9c300
Brandon Wood <brw422@g.harvard.edu>
2023-09-30 13:47:37 -0400

    added ui timer and clown count.

--- scripts/drive_timer.gd
+++ scripts/drive_timer.gd
@@ -1,7 +1,19 @@
 extends Control
 
+@export var time = 60
+
+func _ready():
+	$Timer.wait_time = time
+
 func timesUp():
 	print("You lose")
 
 func _process(delta):
-	$TimeRemaining.text = "Time Remaining\n" + str($Timer.time_left)
+	$TimeRemaining.text = "Time Remaining\n" + str(floor($Timer.time_left)) + "s"
+	
+func start():
+	$Timer.start()
+
+
+func _on_timer_timeout():
+	timesUp()
--- scripts/world.gd
+++ scripts/world.gd
@@ -1,13 +1,17 @@
 extends Node3D
 
 @onready var clowns = get_tree().get_nodes_in_group("clowns")  
-
+@onready var clownCountText = $UI/ClownCount
 
 func _ready():
-	pass
-	#parentCamera()
+	$UI/DriveTimer.start()
 
 func _process(delta):
+	var disabled_clown_count = 0
+	for clown in clowns:
+		if !clown.enabled:
+			disabled_clown_count += 1
+	clownCountText.text = "Clowns " + str(disabled_clown_count) + "/" + str(clowns.size())
 	if victory():
 		print("You win!")
 
